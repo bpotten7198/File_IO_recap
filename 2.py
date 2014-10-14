53f4f9f717150b5e41c640ca1958a4074ddd5408
bpotten7198 <bpotten7198@sohamcollege.org.uk>
2014-10-14 09:47:45 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,5 @@
 #Write a program which takes a list of six names and writes them to a file. Check that the file contains the six names on six separate lines.
+Names = ["Potato","is","love",",","potato","is","life"]
+with open("Shoppinglist.txt", mode="w",encoding="utf-8") as SwegLikeNames:
+    for x in Names:
+          SwegLikeNames.write(x+" ")
